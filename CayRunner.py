b96d00711fd3f8f3df7f2c03fd1ded71c3eaf8b4
Eddie Ruano <eddieruano@gmail.com>
2017-06-16 08:03:47 -0700

    Script

--- CayRunner.py
+++ CayRunner.py
@@ -11,7 +11,7 @@ class CayenneRunner(object):
         self.SendInterval = 10
     def MainLoop(self):
         self.Client.on_message = self.on_message
-        Client.begin(self.MQTT_USERNAME, self.MQTT_PASSWORD, self.MQTT_CLIENT_ID)
+        self.Client.begin(self.MQTT_USERNAME, self.MQTT_PASSWORD, self.MQTT_CLIENT_ID)
         while True:
             self.Client.loop()
             # Get Current Time
